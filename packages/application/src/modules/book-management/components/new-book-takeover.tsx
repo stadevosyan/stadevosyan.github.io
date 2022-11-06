5049125be213deee8da933fe7bb18c2684483e71
adadayan <adadayan@servicetitan.com>
2022-11-06 15:02:38 +0400

    add new book layout changes

--- packages/application/src/modules/book-management/components/new-book-takeover.tsx
+++ packages/application/src/modules/book-management/components/new-book-takeover.tsx
@@ -1,4 +1,13 @@
-import { Button, ButtonGroup, Takeover } from '@servicetitan/design-system';
+import {
+    BodyText,
+    Button,
+    ButtonGroup,
+    Divider,
+    Form,
+    Layout,
+    Stack,
+    Takeover,
+} from '@servicetitan/design-system';
 import { observer } from 'mobx-react';
 import { useDependencies } from '@servicetitan/react-ioc';
 import { NewBookStore } from '../stores/new-book.store';
@@ -9,6 +18,7 @@ export const NewBookTakeover = observer(() => {
     return (
         <Takeover
             portal={false}
+            theme="light"
             open={newBookStore.open}
             focusTrapOptions={{ disabled: true }}
             title="Ավելացնել նոր գիրք"
@@ -22,10 +32,63 @@ export const NewBookTakeover = observer(() => {
             onBack={newBookStore.handleClose}
             onClose={newBookStore.handleClose}
         >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id accumsan augue.
-            Phasellus consequat augue vitae tellus tincidunt posuere. Curabitur justo urna,
-            consectetur vel elit iaculis, ultrices condimentum risus. Nulla facilisi. Etiam
-            venenatis molestie tellus. Quisque consectetur non risus eu rutrum.
+            <Layout type="island">
+                <Stack direction="column">
+                    <Stack.Item>
+                        <Button iconName="add_a_photo">Կցել գրքի լուսանկարը</Button>
+                    </Stack.Item>
+                    <Divider className="m-y-3" />
+                    <Form>
+                        <Form.Group widths="equal" className="">
+                            <Form.Input
+                                label="Գրքի անուն"
+                                /*
+                                 * value={login.value}
+                                 * onChange={login.onChangeHandler}
+                                 * error={login.hasError}
+                                 */
+                            />
+                            <Form.Input
+                                label="Հեղինակի անուն"
+                                /*
+                                 * value={login.value}
+                                 * onChange={login.onChangeHandler}
+                                 * error={login.hasError}
+                                 */
+                            />
+                        </Form.Group>
+                        <Form.TextArea
+                            label="Նկարագրություն (ոչ պարտադիր)"
+                            // placeholder="Aren't you a little short for a stormtrooper?"
+                        />
+                        <BodyText className="m-b-2 t-truncate" size="small" bold>
+                            Հասանելիություն
+                        </BodyText>
+                        <Form.Group className="align-items-stretch m-r-1">
+                            <Form.Togglebox
+                                control="radio"
+                                value={1}
+                                /*
+                                 * checked={value === 1}
+                                 * onClick={value => setValue(value)}
+                                 */
+                                title="Հասանելի"
+                                label=""
+                            />
+                            <Form.Togglebox
+                                control="radio"
+                                value={1}
+                                /*
+                                 * checked={value === 1}
+                                 * onClick={value => setValue(value)}
+                                 */
+                                title="Վարձակալված"
+                                label=""
+                            />
+                        </Form.Group>
+                    </Form>
+                </Stack>
+            </Layout>
         </Takeover>
     );
 });
