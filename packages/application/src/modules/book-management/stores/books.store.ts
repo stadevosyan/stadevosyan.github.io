e8cb7e3757191036479e46f0515ba81c52cf93dd
samveltadevosyan <stadevosyan@servicetitan.com>
2022-11-24 01:43:11 +0400

    books order fix, style fixes

--- packages/application/src/modules/book-management/stores/books.store.ts
+++ packages/application/src/modules/book-management/stores/books.store.ts
@@ -111,7 +111,7 @@ export class BooksStore {
             );
 
             runInAction(() => {
-                this.books = books.data;
+                this.books = books.data.sort((item1, item2) => item2.id - item1.id);
                 this.count = books.count;
             });
 
